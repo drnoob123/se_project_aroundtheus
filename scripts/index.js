a60e7341c0357f75c99f52b0bafa29b9c6a33f3f
Cory <coryligginsla@gmail.com>
2024-02-06 21:57:37 -0800

    design updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
-// Initial cards 
-const initialCards = Array[
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
