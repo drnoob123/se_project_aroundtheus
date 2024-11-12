ecd41629fcc80302748ad9d8d3b37f0d91a049ad
Cory <coryligginsla@gmail.com>
2024-11-12 14:18:03 -0800

    card class

--- components/Card.js
+++ components/Card.js
@@ -1,54 +1,35 @@
 class Card {
-    constructor(name, link , cardSelector, handleImageClick) {
+    constructor(name, link, cardSelector, handleImageClick) {
       this._name = name;
       this._link = link;
       this._cardSelector = cardSelector;
       this._handleImageClick = handleImageClick;
-    }
-  
-    // Private method to get the card template
-    _getTemplate() {
-      const cardElement = document
-        .querySelector(this._cardSelector)
-        .content
-        .firstElementChild
-        .cloneNode(true);
-  
-      return cardElement;
-    }
-  
-    // Public method to create and return a fully functional card element
-    generateCard() {
       this._element = this._getTemplate();
-      this._cardImage = this._element.querySelector(".card__image");
-      this._cardTitle = this._element.querySelector(".card__title");
-      this._likeButton = this._element.querySelector(".card__like-button");
-      this._deleteButton = this._element.querySelector(".card__delete-button");
+      this._setContent();
+      this._addEventListeners();
+    }
   
-      this._cardTitle.textContent = this._name;
-      this._cardImage.src = this._link;
-      this._cardImage.alt = `${this._name} - Image of ${this._name}`;
+    _getTemplate() {
+      return document.querySelector(this._cardSelector).content.firstElementChild.cloneNode(true);
+    }
   
-      this._setEventListeners();
+    _setContent() {
+      const image = this._element.querySelector(".card__image");
+      this._element.querySelector(".card__title").textContent = this._name;
+      image.src = this._link;
+      image.alt = `${this._name} - Image of ${this._name}`;
+    }
   
+    _addEventListeners() {
+      this._element.querySelector(".card__like-button").addEventListener("click", () => 
+        this._element.querySelector(".card__like-button").classList.toggle("card__like-button_active")
+      );
+      this._element.querySelector(".card__delete-button").addEventListener("click", () => this._element.remove());
+      this._element.querySelector(".card__image").addEventListener("click", () => this._handleImageClick(this));
+    }
+  
+    generateCard() {
       return this._element;
-    }
-  
-    // Private method to add event listeners
-    _setEventListeners() {
-      this._likeButton.addEventListener("click", () => this._toggleLike());
-      this._deleteButton.addEventListener("click", () => this._handleDelete());
-      this._cardImage.addEventListener("click", () => this._handleImageClick(this));
-    }
-  
-    // Private method to toggle the like button state
-    _toggleLike() {
-      this._likeButton.classList.toggle("card__like-button_active");
-    }
-  
-    // Private method to handle the card deletion
-    _handleDelete() {
-      this._element.remove();
     }
   }
   
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 // This array holds the initial card data for our gallery. Each item is an object containing:
 // - "name": the name of the place.
 // - "link": the URL to the image of that place.
-import Card from "../components/Card.js";
-import FormValidator from "../components/formValidator.js";
+
 const initialCards = [
   { name: "Yosemite Valley", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg" },
   { name: "Lake Louise", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg" },
